0b72fff4b25a174373230865cc8b6e88023986ab
Brendo <brendobomfim@gmail.com>
2019-08-27 22:04:46 -0300

    Removing unused components and adding the basic mechanic

---
+++ BrackeysGameJam/Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 10f;
+        moveDirection = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.z = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        
+        transform.Translate(moveDirection * Time.deltaTime * moveSpeed);
+    }
+}
