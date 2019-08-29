41b27c469e686cf6ef18ffbbf7bfecada1cd03a2
Brendo <brendobomfim@gmail.com>
2019-08-29 00:40:14 -0300

    Added the colors switch

--- BrackeysGameJam/Assets/PlayerColors.cs
+++ BrackeysGameJam/Assets/PlayerColors.cs
@@ -7,11 +7,26 @@ public class PlayerColors : MonoBehaviour
     public Color trueColor;
     public Color visibleColor;
 
+    private Material material;
+
     void Start()
     {
-        Material material = new Material(Shader.Find("Transparent/Diffuse"));
+        material = new Material(Shader.Find("Transparent/Diffuse"));
         material.color = visibleColor;
 
         GetComponent<Renderer>().material = material;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "newColor")
+        {
+            trueColor = collider.gameObject.GetComponent<ColorChange>().trueColor;
+            visibleColor = collider.gameObject.GetComponent<ColorChange>().visibleColor;
+
+            material.color = visibleColor;
+
+            GetComponent<Renderer>().material = material;
+        }
     }
 }
