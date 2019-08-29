d6da495b2834c7fbe71d1575aacfcdd730f2820e
Brendo <brendobomfim@gmail.com>
2019-08-29 00:15:17 -0300

    Changed the basic core mechanic
    instead of using just physics its now using colors

---
+++ BrackeysGameJam/Assets/CubeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public Color color;
+    void Start()
+    {
+        Material material = new Material(Shader.Find("Transparent/Diffuse"));
+        material.color =  color;
+
+        GetComponent<Renderer>().material = material;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            Color playerColor = collision.gameObject.GetComponent<PlayerColors>().trueColor;
+
+            if (color.IsEqualTo(playerColor))
+            {
+                Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+                rigidbody.useGravity = true;
+                rigidbody.constraints = RigidbodyConstraints.None;
+            }
+            
+
+        }
+    }
+
+}
---
+++ BrackeysGameJam/Assets/Extension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class Extension
+{
+    public static bool IsEqualTo(this Color my, Color other)
+    {
+        return ColorUtility.ToHtmlStringRGBA(my) == ColorUtility.ToHtmlStringRGBA(other);
+    }
+}
