5e6618d560cf6937264e49b33c71997dba1a3c22
Brendo <brendobomfim@gmail.com>
2019-08-31 13:18:52 -0300

    Added a simple dialogue system

--- BrackeysGameJam/Assets/DialogueControl.cs
+++ BrackeysGameJam/Assets/DialogueControl.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
+using System;
 
 public class DialogueControl : MonoBehaviour
 {
-    public TextMeshProUGUI levelText;
-    private string dialogueText;
     public RectTransform panelRectTransform;
+    public float growX = 4f;
+    public float growY = 0.2f;
+    public int dismissTime = 2;
+    public int repeatTimes = 100;
 
     void Start()
     {
-        dialogueText = levelText.text;
-        //levelText.
-        //levelText.text = "";
-        //gameObject.SetActive(true);
-        panelRectTransform.sizeDelta = new Vector2(panelRectTransform.sizeDelta.x, panelRectTransform.sizeDelta.y);
+        StartCoroutine(InvokeMethod(ScalePanel, dismissTime, repeatTimes));
+    }
 
+    private void ScalePanel()
+    {
+        panelRectTransform.sizeDelta = new Vector2(panelRectTransform.sizeDelta.x + growX, panelRectTransform.sizeDelta.y + growY);
     }
 
 
+    public IEnumerator InvokeMethod(Action method, float dismissTime, int invokeCount)
+    {
+        for (int i = 0; i < invokeCount; i++)
+        {
+            method();
+
+            yield return new WaitForFixedUpdate();
+        }
+        yield return new WaitForSeconds(dismissTime);
+        gameObject.SetActive(false);
+    }
 }
+
+
