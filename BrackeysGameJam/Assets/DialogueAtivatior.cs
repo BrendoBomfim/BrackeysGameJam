4ddbfbe690aa27fb4c0fbe5b06a62a784a7534e8
Brendo <brendobomfim@gmail.com>
2019-08-31 14:05:51 -0300

    Finished the second level dialog

---
+++ BrackeysGameJam/Assets/DialogueAtivatior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAtivatior : MonoBehaviour
+{
+    private bool wasActivated;
+    public RectTransform panelRectTransform;
+    private void Start()
+    {
+        wasActivated = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !wasActivated)
+        {
+            wasActivated = true;
+            panelRectTransform.gameObject.SetActive(true);
+        }
+    }
+}
