e5e5b8b3ff62481e647c35743f0c9287ce77cfd6
Brendo <brendobomfim@gmail.com>
2019-08-31 13:00:05 -0300

    Changed the menu font and added functions to the button

---
+++ BrackeysGameJam/Assets/DialogueControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DialogueControl : MonoBehaviour
+{
+    public TextMeshProUGUI levelText;
+    private string dialogueText;
+    public RectTransform panelRectTransform;
+
+    void Start()
+    {
+        dialogueText = levelText.text;
+        //levelText.
+        //levelText.text = "";
+        //gameObject.SetActive(true);
+        panelRectTransform.sizeDelta = new Vector2(panelRectTransform.sizeDelta.x, panelRectTransform.sizeDelta.y);
+
+    }
+
+
+}
